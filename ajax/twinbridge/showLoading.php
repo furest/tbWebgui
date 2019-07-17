0470925dbcebe5960895c0c1ddca77dc27320ac4
furest <yannick.diepart@hotmail.com>
2019-07-17 23:10:06 +0200

    Update showLoading.php

--- ajax/twinbridge/showLoading.php
+++ ajax/twinbridge/showLoading.php
@@ -6,7 +6,7 @@
     ?>
     <h4><?php echo _("Connection in progress");?></h4>
     <div class="btn-group btn-block">
-      <a href="#" style="padding:10px;float: right;display: block;position: relative;margin-top: -55px;" class="col-md-2 btn btn-danger" id="kill" onclick="killVPN()" csrf="<?php echo htmlspecialchars($_SESSION['csrf_token'], ENT_QUOTES);?>">Disconnect</a>
+      <a href="#" style="padding:10px;float: right;display: block;position: relative;margin-top: -45px;" class="col-md-2 btn btn-danger" id="kill" onclick="killVPN()" csrf="<?php echo htmlspecialchars($_SESSION['csrf_token'], ENT_QUOTES);?>">Disconnect</a>
     </div>
     <div align="center">
         <img src="/img/loading.svg"> 
